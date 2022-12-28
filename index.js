2347ebe0468f40cbd7867408d0c0ed0579e21c35
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2022-12-28 12:35:25 +0100

    Promote Colette bot

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const Bot = require('./classes/Bot')
 const config = require('./config/settings')
 const figlet = require('figlet')
 
-
 console.log(figlet.textSync('Gemify Tool'))
+console.log('')
+console.log('> Before to use this tool to gemify your items...')
+console.log('> Please, consider to sell them on Colette gems bot.')
+console.log('> See https://steamcommunity.com/profiles/76561199096164013')
+console.log('')
 
 new Bot(config).run()
