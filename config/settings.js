7bb4a714a32cceaac6cb5ae52e965cacdd9eed56
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2022-12-28 12:35:25 +0100

    Remove unnecessary 'identity_secret' from settings

--- config/settings.js
+++ config/settings.js
@@ -4,7 +4,6 @@ module.exports = {
     "username": '',             // Steam username
     "password": '',             // Steam password
     "shared_secret": '',        // Steam Guard (aka 2FA) shared secret
-    "identity_secret": '',      // Steam Guard (aka 2FA) indentity secret
 
     // GEMIFY CONFIG
     "MAX_STOCK": 0,             // Keep up to XX copies on Steam inventory
